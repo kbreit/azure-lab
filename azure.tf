d550c24ed40c0de0b6afd247f8f3b97271697dac
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-03 16:54:09 -0500

    First pass at VNET

--- azure.tf
+++ azure.tf
@@ -5,4 +5,19 @@ provider "azurerm" {
 resource "azurerm_resource_group" "azurerm_resource_group" {
   name = var.azure_rg_name
   location = var.azure_rg_region
+
+  tags = {
+    owner = "Kevin Breit"
+  }
+
+}
+
+module "network" {
+  source = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.azurerm_resource_group.name
+  address_spaces = var.azure_vnet_address_spaces
+
+  tags = {
+    owner = "Kevin Breit"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,10 @@ variable "azure_rg_region" {
   type = string
   default="centralus"
 }
+
+variable "azure_vnet_address_spaces" {
+  type = list(string)
+  default = [
+    "10.2.0.0/16"
+  ]
+}
