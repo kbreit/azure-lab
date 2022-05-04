d220d11d41b52a0bc7ac51fcbe51aef17c883533
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-04 10:51:23 -0500

    Add second VNET

--- azure.tf
+++ azure.tf
@@ -15,12 +15,21 @@ resource "azurerm_resource_group" "azurerm_resource_group" {
 module "network" {
   source = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.azurerm_resource_group.name
-  address_spaces = var.azure_vnet_address_spaces
-  vnet_name = var.azure_vnet_name
-  subnet_prefixes = var.azure_vnet_subnets
-  subnet_names = var.azure_subnet_names
+  address_spaces = var.azure_vnet_lz_address_spaces
+  vnet_name = var.azure_vnet_lz_name
+  subnet_prefixes = var.azure_vnet_lz_subnets
+  subnet_names = var.azure_subnet_lz_names
 
   tags = {
     owner = "Kevin Breit"
   }
+}
+
+module "network" {
+  source = "Azure/network/azurerm"
+  resource_group_name =  azurerm_resource_group.azurerm_resource_group.name
+  address_spaces = var.azure_vnet_app_address_spaces
+  vnet_name = var.azure_vnet_app_name
+  subnet_prefixes = var.azure_vnet_app_subnets
+  subnet_names = var.azure_app_subnet_names
 }
--- variables.tf
+++ variables.tf
@@ -8,28 +8,52 @@ variable "azure_rg_region" {
   default="centralus"
 }
 
-variable "azure_vnet_address_spaces" {
+variable "azure_vnet_lz_address_spaces" {
   type = list(string)
   default = [
     "10.2.0.0/16"
   ]
 }
 
-variable "azure_vnet_subnets" {
+variable "azure_vnet_lz_subnets" {
   type = list(string)
   default = [
     "10.2.1.0/24"
   ]
 }
 
-variable "azure_subnet_names" {
+variable "azure_lz_subnet_names" {
   type = list(string)
   default = [
     "subnet1"
   ]
 }
 
-variable "azure_vnet_name" {
+variable "azure_vnet_lz_name" {
   type = string
   default = "kbreit-default"
+}
+
+variable "azure_vnet_app_address_spaces" {
+  type = list(string)
+  default = [
+    "10.3.0.0/16"
+  ]
+}
+
+variable "azure_vnet_app_subnets" {
+  type = list(string)
+  default = [
+    "10.3.1.0/24"
+  ]
+}
+
+varialbe "azure_app_subnet_names" {
+  type = list(string)
+  default = ["web"]
+}
+
+variable "azure_vnet_app_name" {
+  type = string
+  default = "kbreit-app"
 }
