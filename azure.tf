8226196342c91545da865359bd2ef0050674ff91
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-04 12:53:37 -0500

    First attempt at VPN gateway

--- azure.tf
+++ azure.tf
@@ -49,3 +49,31 @@ resource "azurerm_virtual_network_peering" "app_to_default" {
   virtual_network_name =  module.vnet_app.vnet_name
   remote_virtual_network_id = module.vnet_default.vnet_id
 }
+
+resource "azurerm_public_ip" "vpn_gateway_ip" {
+  name = "vpn-gateway-ip"
+  location = azurerm_resource_group.azurerm_resource_group.location
+  resource_group_name = azurerm_resource_group.azurerm_resource_group.name
+
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "vpn_gateway" {
+  name = "vpn-gateway"
+  location = azurerm_resource_group.azurerm_resource_group.location
+  resource_group_name = azurerm_resource_group.azurerm_resource_group.name
+
+  type = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp = false
+  sku = "Basic"
+
+  ip_configuration {
+    name = "vpnGatewayConfig"
+    public_ip_address_id = azure_public_ip.vpn_gateway_ip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id = module.vnet_default.subnets[0]
+  }
+}
