653497c868161f985dafc6ddf289a04791cbfb94
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-04 11:11:09 -0500

    Add VNet peering

--- azure.tf
+++ azure.tf
@@ -14,6 +14,7 @@ resource "azurerm_resource_group" "azurerm_resource_group" {
 
 module "vnet_default" {
   source = "Azure/network/azurerm"
+  version = "3.5.0"
   resource_group_name = azurerm_resource_group.azurerm_resource_group.name
   address_spaces = var.azure_vnet_lz_address_spaces
   vnet_name = var.azure_vnet_lz_names
@@ -27,9 +28,17 @@ module "vnet_default" {
 
 module "vnet_app" {
   source = "Azure/network/azurerm"
+  version = "3.5.0"
   resource_group_name =  azurerm_resource_group.azurerm_resource_group.name
   address_spaces = var.azure_vnet_app_address_spaces
   vnet_name = var.azure_vnet_app_names
   subnet_prefixes = var.azure_vnet_app_subnets
   subnet_names = var.azure_app_subnet_names
+}
+
+resource "azurerm_virtual_network_peering" "default_to_app" {
+  name = "default-to-app"
+  resource_group_name = azurerm_resource_group.azurerm_resource_group.name
+  virtual_network_name =  module.vnet_default.vnet_name
+  remote_virtual_network_id = module.vnet_app.vnet_name
 }
