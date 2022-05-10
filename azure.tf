bd08199813d6adf2f9d9831da8d62e52ed1004e6
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-10 10:00:19 -0500

    Re-enable common VNet route table

--- azure.tf
+++ azure.tf
@@ -342,18 +342,18 @@ resource "azurerm_route_table" "firewall_table" {
   }
 }
 
-# resource "azurerm_route_table" "common-table" {
-#   name = "common-table"
-#   resource_group_name = azurerm_resource_group.azurerm_resource_group.name
-#   location            = azurerm_resource_group.azurerm_resource_group.location
+resource "azurerm_route_table" "common-table" {
+  name = "common-table"
+  resource_group_name = azurerm_resource_group.azurerm_resource_group.name
+  location            = azurerm_resource_group.azurerm_resource_group.location
 
-#   route {
-#     name = "app-route"
-#     address_prefix = var.azure_vnet_app_address_spaces[0]
-#     next_hop_type = "VirtualAppliance"
-#     next_hop_in_ip_address = azurerm_firewall.hub_firewall.ip_configuration[0].private_ip_address
-#   }
-# }
+  route {
+    name = "app-route"
+    address_prefix = var.azure_vnet_app_address_spaces[0]
+    next_hop_type = "VirtualAppliance"
+    next_hop_in_ip_address = azurerm_firewall.hub_firewall.ip_configuration[0].private_ip_address
+  }
+}
 
 # resource "azurerm_route_table" "app-table" {
 #   name = "app-table"
