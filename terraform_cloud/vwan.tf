63f5f065b501218cb2f65d1d42c4e71e9f17a998
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-16 10:53:33 -0500

    Remove VCS repo for workspace

--- terraform_cloud/vwan.tf
+++ terraform_cloud/vwan.tf
@@ -2,24 +2,15 @@ data "tfe_organization" "org" {
     name = var.tfe_org_name
 }
 
-# resource "tfe_oauth_client" "github_client" {
-#     name = "github-oauth-client"
-#     organization = data.tfe_organization.org.name
-#     api_url = "https://api.github.com"
-#     http_url = "https://github.com"
-#     oauth_token = var.github_oauth_token
-#     service_provider = "github"
-# }
-
 resource "tfe_workspace" "vwan" {
     name = var.tfe_workspace_vwan_name
     organization = data.tfe_organization.org.name
     auto_apply = false
     working_directory = "vwan"
-    vcs_repo {
-        branch = ""
-        identifier = "kbreit/azure-lab"
-        ingress_submodules = false
-        oauth_token_id = var.github_oauth_token_id
-    }
+    # vcs_repo {
+    #     branch = ""
+    #     identifier = "kbreit/azure-lab"
+    #     ingress_submodules = false
+    #     oauth_token_id = var.github_oauth_token_id
+    # }
 }
