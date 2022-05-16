afd52779bee633778817c5313a22adb2f7fd4108
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-16 10:39:52 -0500

    Create Github Oauth client with real token

--- terraform_cloud/vwan.tf
+++ terraform_cloud/vwan.tf
@@ -8,16 +8,17 @@ resource "tfe_oauth_client" "github_client" {
     api_url = "https://api.github.com"
     http_url = "https://github.com"
     oauth_token = var.github_oauth_token
+    service_provider = "github"
 }
 
-resource "tfe_workspace" "vwan" {
-    name = var.tfe_workspace_vwan_name
-    organization = data.tfe_organization.org.name
-    auto_apply = false
-    working_directory = "vwan"
-    vcs_repo {
-        branch = ""
-        identifier = "kbreit/azure-lab"
-        ingress_submodules = false
-    }
-}
+# resource "tfe_workspace" "vwan" {
+#     name = var.tfe_workspace_vwan_name
+#     organization = data.tfe_organization.org.name
+#     auto_apply = false
+#     working_directory = "vwan"
+#     vcs_repo {
+#         branch = ""
+#         identifier = "kbreit/azure-lab"
+#         ingress_submodules = false
+#     }
+# }
