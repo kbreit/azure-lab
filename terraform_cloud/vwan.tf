7d2ad2281fdc237942808d207cca5a32c43a63ea
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-16 10:09:18 -0500

    Create VWAN TFE workspace

--- terraform_cloud/vwan.tf
+++ terraform_cloud/vwan.tf
@@ -11,11 +11,14 @@ output "workspace_name" {
     value = data.tfe_workspace.traditional_vnet
 }
 
-# resource "tfe_workspace" "vwan" {
-#     name = var.tfe_workspace_vwan_name
-#     organization = data.tfe_organization.org.name
-#     auto_apply = false
-#     vc_repo {
-
-#     }
-# }
+resource "tfe_workspace" "vwan" {
+    name = var.tfe_workspace_vwan_name
+    organization = data.tfe_organization.org.name
+    auto_apply = false
+    working_directory = "vwan"
+    vc_repo {
+        branch = ""
+        identifier = "kbreit/azure-lab"
+        ingress_submodules = false
+    }
+}
