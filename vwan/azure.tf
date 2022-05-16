bc66e450da49f6ee60cd7666bd8f8de3c15317da
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-16 16:37:39 -0500

    Create vnets for vwan RG

--- vwan/azure.tf
+++ vwan/azure.tf
@@ -16,3 +16,47 @@ resource "azurerm_virtual_wan" "vwan" {
   resource_group_name = azurerm_resource_group.kbreit-vwan-rg.name
   location = azurerm_resource_group.kbreit-vwan-rg.location
 }
+
+module "vnet_default" {
+  source = "Azure/vnet/azurerm"
+  version = "2.6.0"
+  resource_group_name = azurerm_resource_group.vwan.name
+  address_space = var.azure_vnet_lz_address_spaces
+  vnet_name = var.azure_vnet_lz_names
+  subnet_prefixes = var.azure_vnet_lz_subnets
+  subnet_names = var.azure_lz_subnet_names
+
+  nsg_ids = {
+    subnet1 = module.default-nsg.network_security_group_id
+  }
+
+  route_tables_ids = {
+    subnet1 = azurerm_route_table.firewall_table.id
+  }
+
+  tags = {
+    owner = "Kevin Breit"
+  }
+}
+
+module "vnet_app" {
+  source = "Azure/vnet/azurerm"
+  version = "2.6.0"
+  resource_group_name =  azurerm_resource_group.vwan.name
+  address_space = var.azure_vnet_app_address_spaces
+  vnet_name = var.azure_vnet_app_names
+  subnet_prefixes = var.azure_vnet_app_subnets
+  subnet_names = var.azure_app_subnet_names
+
+  route_tables_ids = {
+    web = azurerm_route_table.app_table.id
+  }
+
+  nsg_ids = {
+    web = module.default-nsg.network_security_group_id
+  }
+
+  tags = {
+    owner = "Kevin Breit"
+  }
+}
--- vwan/variables.tf
+++ vwan/variables.tf
@@ -7,3 +7,59 @@ variable "azure_rg_region" {
   type = string
   default="centralus"
 }
+
+variable "azure_vnet_lz_address_spaces" {
+  type = list(string)
+  default = [
+    "10.2.0.0/16"
+  ]
+}
+
+variable "azure_vnet_lz_subnets" {
+  type = list(string)
+  default = [
+    "10.2.1.0/24",
+    "10.2.2.0/24",
+    "10.2.3.0/24",
+    "10.2.4.0/24",
+  ]
+}
+
+variable "azure_lz_subnet_names" {
+  type = list(string)
+  default = [
+    "subnet1",
+    "GatewaySubnet",
+    "AzureBastionSubnet",
+    "AzureFirewallSubnet",
+  ]
+}
+
+variable "azure_vnet_lz_names" {
+  type = string
+  default = "kbreit-default"
+}
+
+variable "azure_vnet_app_address_spaces" {
+  type = list(string)
+  default = [
+    "10.3.0.0/16"
+  ]
+}
+
+variable "azure_vnet_app_subnets" {
+  type = list(string)
+  default = [
+    "10.3.1.0/24"
+  ]
+}
+
+variable "azure_app_subnet_names" {
+  type = list(string)
+  default = ["web"]
+}
+
+variable "azure_vnet_app_names" {
+  type = string
+  default = "kbreit-app"
+}
