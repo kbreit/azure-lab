e41b522f0b0c684ec2b96b57a2abab46202dc2e0
Kevin Breit <kevin.breit@kevinbreit.net>
2022-05-16 15:32:12 -0500

    Fix name of RG

--- vwan/azure.tf
+++ vwan/azure.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "azurerm_resource_group" {
+resource "azurerm_resource_group" "kbreit-vwan-rg" {
   name = var.azure_rg_name
   location = var.azure_rg_region
 
